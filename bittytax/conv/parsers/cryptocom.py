c9261fe42be59f49af1b8eabaafdbf3bb64a06d6
aleqx <aleqx.git.nospam@bemail.one>
2021-02-21 04:22:58 +0000

    rewrite the Crypto.com parser to deal with swaps and dust conversions (both important)

--- bittytax/conv/parsers/cryptocom.py
+++ bittytax/conv/parsers/cryptocom.py
@@ -1,18 +1,67 @@
 # -*- coding: utf-8 -*-
 # (c) Nano Nano Ltd 2020
+# (c) aleqx on github 2021
+
+# TODO: (aleqx)
+#  - The types "dynamic_coin_swap_{credited,debited}" seem to be duplicates
+#    of "crypto_wallet_swap_{credited,debited}", hence just ignore.
+#  - The type "dynamic_coin_swap_bonus_exchange_deposit" seems redundant as it should
+#    be present on the exchange records, not the app's, hence ignore.
+#  - The types "lockup_swap_credited", "interest_swap_credited" do _not_ seem redundant.
+#  Revisit if any evidence is presented to the contrary of any of the above.
 
 from decimal import Decimal
+from functools import reduce
 
 from ...config import config
 from ..out_record import TransactionOutRecord
 from ..dataparser import DataParser
-from ..exceptions import UnexpectedTypeError
+from ..exceptions import DataParserError, UnexpectedTypeError
 
 WALLET = "Crypto.com"
 
+def parse_crypto_com_all(data_rows, parser, _filename):
+    # order chronologically
+    data_rows.sort(key=lambda row: row.in_row[0])
+    debited_rows = []
+    credited_row = {}
+    # Swaps and dust conversions span multiple "debited" and "credited" rows;
+    # there are multiple "debited" rows in a dust conversion; the order of
+    # debited/credited can be arbitrary; hence we gobble till we hit non-swap rows.
+    for data_row in data_rows:
+        try:
+            in_row = data_row.in_row
+            data_row.timestamp = DataParser.parse_timestamp(in_row[0])
+            if in_row[9] in ("dust_conversion_credited", "crypto_wallet_swap_credited",
+                             "lockup_swap_credited", "interest_swap_credited"):
+                credited_row = data_row
+                continue
+            elif in_row[9] in ("dust_conversion_debited", "crypto_wallet_swap_debited",
+                               "lockup_swap_debited", "interest_swap_debited"):
+                debited_rows.append(data_row)
+                continue
+            elif len(debited_rows) == 0 or not credited_row:
+                parse_crypto_com(data_row, parser, _filename)
+            else:  # hit a non-swap row after a sequence of swap rows
+                total_debited_native = reduce(lambda x, y: x + y, [Decimal(data_row.in_row[7]) for data_row in debited_rows])
+                for debited_row in debited_rows:
+                    debited_row.in_row[4] = credited_row.in_row[2]
+                    debited_row.in_row[5] = str(Decimal(credited_row.in_row[3]) * Decimal(debited_row.in_row[7]) / total_debited_native) \
+                        if total_debited_native > 0 else str(Decimal(credited_row.in_row[3]) / len(debited_rows))
+                    debited_row.in_row[9] = debited_row.in_row[9].replace('_debited', '')  # rename type
+                    parse_crypto_com(debited_row, parser, _filename)
+                debited_rows = []
+                credited_row = {}
+                # parse the non-swap row too
+                parse_crypto_com(data_row, parser, _filename)
+
+        except DataParserError as e:
+            data_row.failure = e
+
+
 def parse_crypto_com(data_row, parser, _filename):
     in_row = data_row.in_row
-    data_row.timestamp = DataParser.parse_timestamp(in_row[0])
+    # data_row.timestamp = DataParser.parse_timestamp(in_row[0])
 
     if in_row[9] == "crypto_transfer":
         if Decimal(in_row[3]) > 0:
@@ -38,7 +87,8 @@ def parse_crypto_com(data_row, parser, _filename):
                                                  buy_value=get_value(in_row),
                                                  wallet=WALLET)
     elif in_row[9] in ("viban_purchase", "van_purchase",
-                       "crypto_viban_exchange", "crypto_exchange"):
+                       "crypto_viban_exchange", "crypto_exchange",
+                       "dust_conversion", "crypto_wallet_swap"):
         data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_TRADE,
                                                  data_row.timestamp,
                                                  buy_quantity=in_row[5],
@@ -47,7 +97,7 @@ def parse_crypto_com(data_row, parser, _filename):
                                                  sell_asset=in_row[2],
                                                  sell_value=get_value(in_row),
                                                  wallet=WALLET)
-    elif in_row[9] in ("crypto_purchase", "dust_conversion_debited", "dust_conversion_credited"):
+    elif in_row[9] in ("crypto_purchase"):
         if Decimal(in_row[3]) > 0:
             data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_TRADE,
                                                      data_row.timestamp,
@@ -66,7 +116,7 @@ def parse_crypto_com(data_row, parser, _filename):
                                                      wallet=WALLET)
     elif in_row[9] in ("referral_bonus", "referral_card_cashback", "reimbursement",
                        "gift_card_reward", "transfer_cashback", "admin_wallet_credited",
-                       "referral_gift"):
+                       "referral_gift", "mobile_airtime_reward", "crypto_wallet_swap_credited"):
         data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_GIFT_RECEIVED,
                                                  data_row.timestamp,
                                                  buy_quantity=in_row[3],
@@ -104,11 +154,8 @@ def parse_crypto_com(data_row, parser, _filename):
                                                  wallet=WALLET)
     elif in_row[9] in ("crypto_earn_program_created", "crypto_earn_program_withdrawn",
                        "lockup_lock", "lockup_upgrade",
-                       "lockup_swap_credited", "lockup_swap_debited",
                        "dynamic_coin_swap_credited", "dynamic_coin_swap_debited",
                        "dynamic_coin_swap_bonus_exchange_deposit",
-                       "interest_swap_credited", "interest_swap_debited",
-                       "crypto_wallet_swap_credited", "crypto_wallet_swap_debited",
                        "supercharger_deposit", "supercharger_withdrawal"):
         return
     elif in_row[9] == "":
@@ -139,4 +186,5 @@ DataParser(DataParser.TYPE_EXCHANGE,
             'To Amount', 'Native Currency', 'Native Amount', 'Native Amount (in USD)',
             'Transaction Kind'],
            worksheet_name="Crypto.com",
-           row_handler=parse_crypto_com)
+           # row_handler=parse_crypto_com)
+           all_handler=parse_crypto_com_all)
